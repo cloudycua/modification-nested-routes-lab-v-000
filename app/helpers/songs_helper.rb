d15e519fbbafac521e740cfe76bef5ab8be55ab8
Christine Cua <cloudycua@gmail.com>
2018-10-30 20:02:05 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
   def artist_select(artist, song)
     if artist
-      artist.name
+      hidden_field_tag "song[artist_id]", song.artist_id
     else
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
